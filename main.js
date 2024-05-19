9e6597c9b64a162c4051c837f21f09a3885b7a85
Victor Hernandez <victormanu968@gmail.com>
2024-05-19 18:22:03 -0500

    basic pure js front

---
+++ main.js
@@ -0,0 +1,123 @@
+const table = document.querySelector('#users-table tbody');
+
+
+const editModal = new bootstrap.Modal(document.getElementById('editModal'), {});
+const editForm = document.querySelector('#edit-form');
+const editId = document.querySelector('#edit-id');
+const editName = document.querySelector('#edit-name');
+const editEmail = document.querySelector('#edit-email');
+const editCompany = document.querySelector('#edit-company');
+const editCity = document.querySelector('#edit-city');
+
+
+
+const CargarUsuarios = async() => {
+    const array = [];
+    const res = await fetch('data.json');
+    const data = await res.json();
+    data.forEach(user => {
+        array.push(user);
+    });
+    return array;
+}
+
+let usuarios = await CargarUsuarios();
+
+function limpiar() {
+    table.innerHTML = '';
+}
+
+function Eliminar(id) {
+    usuarios = usuarios.filter(user => user.id !== id);
+
+    MostrarUsuarios(usuarios);
+}
+
+function MostrarUsuarios(users) {
+        limpiar();
+         users.forEach(user => {
+            const row = document.createElement('tr')
+            row.innerHTML = `
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.company.name}</td>
+                <td>${user.address.city}</td>
+                <td>
+                <a href="#" class="btn btn-danger btn-delete" data-id="${user.id}">Eliminar</a>
+                <button type="button" class="btn btn-primary btn-edit" data-id="${user.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
+                </td>
+            `;
+            table.appendChild(row);
+});
+
+
+// `
+// <a href="#" class="btn btn-primary btn-edit" data-id="${user.id}">Editar</a>
+// `
+
+
+    //eventos delete
+    const deleteButtons = document.querySelectorAll('.btn-delete');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+            const userId = parseInt(event.target.getAttribute('data-id'));
+            console.log(userId);
+            Eliminar(userId);
+        });
+    });
+
+
+    //eventos edit
+    const editButtons = document.querySelectorAll('.btn-edit');
+    editButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+            const userId = parseInt(event.target.getAttribute('data-id'));
+            Editar(userId);
+        });
+    });
+
+}
+
+
+function Editar(id) {
+    const user = usuarios.find(user => user.id === id);
+    editId.value = user.id;
+    editName.value = user.name;
+    editEmail.value = user.email;
+    editCompany.value = user.company.name;
+    editCity.value = user.address.city;
+    editModal.show();
+}
+
+
+function GuardarCambios(event) {
+    event.preventDefault();
+    const id = parseInt(editId.value);
+    const userIndex = usuarios.findIndex(user => user.id === id);
+    usuarios[userIndex].name = editName.value;
+    usuarios[userIndex].email = editEmail.value;
+    usuarios[userIndex].company.name = editCompany.value;
+    usuarios[userIndex].address.city = editCity.value;
+    editModal.hide();
+    MostrarUsuarios(usuarios);
+}
+
+
+function CancelarEdicion() {
+    editForm.style.display = 'none';
+}
+
+
+document.querySelector('#edit-form').addEventListener('submit', GuardarCambios);
+// editForm.querySelector('form').addEventListener('submit', GuardarCambios);
+// document.querySelector('#cancel-edit').addEventListener('click', CancelarEdicion);
+
+
+
+console.log(usuarios);
+MostrarUsuarios(usuarios);
+
+fetch('http://localhost:8080/api/persons').then(res => res.json()).then(data => console.log(data))
